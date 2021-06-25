ebe2aaa693b3fc3b3b7dbd6a616b9668ae8991f0
Byukusenge Alphonse <alphonso2009@gmail.com>
2021-06-25 07:18:11 +0300

    Manage Product read all product

--- routes/web.php
+++ routes/web.php
@@ -93,6 +93,7 @@ Route::prefix('product')->group(function(){
 
 Route::get('/add', [ProductController::class, 'AddProduct'])->name('add-product');
 Route::post('/store', [ProductController::class, 'StoreProduct'])->name('product-store');
+Route::get('/manage', [ProductController::class, 'ManageProduct'])->name('manage-product');
 
 });
 
