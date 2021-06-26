1e04a0276ffed99820d335d2b1515838cb5ad6fa
Byukusenge Alphonse <alphonso2009@gmail.com>
2021-06-26 20:24:23 +0300

    Slider Active/Desactive

--- app/Http/Controllers/Backend/SliderController.php
+++ app/Http/Controllers/Backend/SliderController.php
@@ -97,4 +97,43 @@ public function SliderUpdate(Request $request){
 
         } // end else 
     } // end method 
+    public function SliderDelete($id){
+        $slider = Slider::findOrFail($id);
+        $img = $slider->slider_img;
+        unlink($img);
+        Slider::findOrFail($id)->delete();
+
+        $notification = array(
+            'message' => 'Slider Delectd Successfully',
+            'alert-type' => 'info'
+        );
+
+        return redirect()->back()->with($notification);
+
+    } // end method
+
+    public function SliderInactive($id){
+        Slider::findOrFail($id)->update(['status' => 0]);
+
+        $notification = array(
+            'message' => 'Slider Inactive Successfully',
+            'alert-type' => 'info'
+        );
+
+        return redirect()->back()->with($notification);
+
+    } // end method 
+
+
+    public function SliderActive($id){
+        Slider::findOrFail($id)->update(['status' => 1]);
+
+        $notification = array(
+            'message' => 'Slider Active Successfully',
+            'alert-type' => 'info'
+        );
+
+        return redirect()->back()->with($notification);
+
+    } // end method 
 }
--- routes/web.php
+++ routes/web.php
@@ -113,6 +113,8 @@ Route::get('/view', [SliderController::class, 'SliderView'])->name('manage-slide
 Route::post('/store', [SliderController::class, 'SliderStore'])->name('slider.store');
 Route::get('/edit/{id}', [SliderController::class, 'SliderEdit'])->name('slider.edit');
 Route::post('/update', [SliderController::class, 'SliderUpdate'])->name('slider.update');
-Route::get('/delete/{id}', [BrandController::class, 'BrandDelete'])->name('brand.delete');
+Route::get('/delete/{id}', [SliderController::class, 'SliderDelete'])->name('slider.delete');
+Route::get('/inactive/{id}', [SliderController::class, 'SliderInactive'])->name('slider.inactive');
+Route::get('/active/{id}', [SliderController::class, 'SliderActive'])->name('slider.active');
 
 });
