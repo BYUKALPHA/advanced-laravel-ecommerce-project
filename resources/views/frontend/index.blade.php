76496478e8f4895589225e8083acbb3a39d4c23c
Byukusenge Alphonse <alphonso2009@gmail.com>
2021-07-12 18:17:53 +0300

    Frontend Product Discount, Product Price show with multiple languages

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -936,9 +936,12 @@
           <div class="more-info-tab clearfix ">
             <h3 class="new-product-title pull-left">New Products</h3>
             <ul class="nav nav-tabs nav-tab-line pull-right" id="new-products-1">
-              <li class="active"><a data-transition-type="backSlide" href="#all" data-toggle="tab">All</a></li>
+              <li class="active"><a data-transition-type="backSlide" href="#all" data-toggle="tab">
+                @if(session()->get('language') == 'french') Tous @else All @endif</
+              </a></li>
    @foreach($categories as $category)
-                <li><a data-transition-type="backSlide" href="#category{{ $category->id }}" data-toggle="tab">{{ $category->category_name_en }}</a></li>
+                <li><a data-transition-type="backSlide" href="#category{{ $category->id }}" data-toggle="tab">
+                  @if(session()->get('language') == 'french') {{ $category->category_name_fr }} @else {{ $category->category_name_en }} @endif</a></li>
            @endforeach
             <!--   <li><a data-transition-type="backSlide" href="#laptop" data-toggle="tab">Electronics</a></li>
               <li><a data-transition-type="backSlide" href="#apple" data-toggle="tab">Shoes</a></li> -->
@@ -1044,16 +1047,34 @@
                         <div class="product-image">
                          <div class="image"> <a href="detail.html"><img  src="{{ asset($product->product_thambnail) }}" alt=""></a> </div>
                           <!-- /.image -->
-                          
-                          <div class="tag new"><span>new</span></div>
-                        </div>
+
+        @php
+        $amount = $product->selling_price - $product->discount_price;
+        $discount = ($amount/$product->selling_price) * 100;
+        @endphp 
+      
+
+                          <div>
+            @if ($product->discount_price == NULL)
+            <div class="tag new"><span>new</span></div>
+            @else
+            <div class="tag hot"><span>{{ round($discount) }}%</span></div>
+            @endif
+          </div>
+                         </div>
                         <!-- /.product-image -->
                         
     <div class="product-info text-left">
-          <h3 class="name"><a href="detail.html">{{ $product->product_name_en }}</a></h3>
+         <h3 class="name"><a href="detail.html">
+@if(session()->get('language') == 'french') {{ $product->product_name_fr }} @else {{ $product->product_name_en }} @endif
+            </a></h3>
           <div class="rating rateit-small"></div>
           <div class="description"></div>
-          <div class="product-price"> <span class="price"> $450.99 </span> <span class="price-before-discount">$ 800</span> </div>
+            @if ($product->discount_price == NULL)
+    <div class="product-price"> <span class="price"> ${{ $product->selling_price }} </span>  </div>
+         @else
+ <div class="product-price"> <span class="price"> ${{ $product->discount_price }} </span> <span class="price-before-discount">$ {{ $product->selling_price }}</span> </div>
+         @endif
           <!-- /.product-price --> 
 
         </div>
