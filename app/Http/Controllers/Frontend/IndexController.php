d0bf844a42e97c9a78d5a715839a90efbe6202ff
Byukusenge Alphonse <alphonso2009@gmail.com>
2021-07-16 17:18:30 +0300

    Update Product Hotdeals Get Only Discount Product

--- app/Http/Controllers/Frontend/IndexController.php
+++ app/Http/Controllers/Frontend/IndexController.php
@@ -20,7 +20,7 @@ class IndexController extends Controller
     $sliders = Slider::where('status',1)->orderBy('id','DESC')->limit(3)->get();
     $categories = Category::orderBy('category_name_en','ASC')->get();
     $featured = Product::where('featured',1)->orderBy('id','DESC')->limit(6)->get();
-    $hot_deals = Product::where('hot_deals',1)->orderBy('id','DESC')->limit(3)->get();
+    $hot_deals = Product::where('hot_deals',1)->where('discount_price','!=',NULL)->orderBy('id','DESC')->limit(3)->get();
     $special_offer = Product::where('special_offer',1)->orderBy('id','DESC')->limit(6)->get();
     $special_deals = Product::where('special_deals',1)->orderBy('id','DESC')->limit(3)->get();
     $skip_category_0 = Category::skip(0)->first();
